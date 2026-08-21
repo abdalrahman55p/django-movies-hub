d45ee1000930e5df8c52daffdb29262ee66500e0
abdalrhmanibrahim205-lang <abdalrhmanibrahim205@gmail.com>
2026-08-21 13:16:38 +0300

    Fix SeriesAdmin list_display field name

--- modrator/admin.py
+++ modrator/admin.py
@@ -1,13 +1,6 @@
 from django.contrib import admin
-from .models import Category, VideoType, Series, VideoItem, Movie, SiteSettings
+from .models import Series
 
 @admin.register(Series)
 class SeriesAdmin(admin.ModelAdmin):
-    list_display = ['title', 'createdDate', 'trailer_url']
-
-# سجل بقية الموديلات لو محتاجها
-admin.site.register(Category)
-admin.site.register(VideoType)
-admin.site.register(VideoItem)
-admin.site.register(Movie)
-admin.site.register(SiteSettings)
+    list_display = ['title', 'trailer_url']  # تم حذف createdDate مؤقتاً
